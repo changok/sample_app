35b916d4e34893b8f14abe07e50337e40c382148
Changok Kim <ckim@twitter.com>
2012-11-05 10:45:04 -0800

    Finish layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 SampleApp::Application.routes.draw do
-  root to: 'static_pages#home'
+  get "users/new"
 
-  match '/help', :to => 'static_pages#help'
-  match '/about',:to => 'static_pages#about'
+  root :to => 'static_pages#home'
+
+  match '/signup',  :to => 'users#new'
+  match '/home',    :to => 'static_pages#home'
+  match '/help',    :to => 'static_pages#help'
+  match '/about',   :to => 'static_pages#about'
   match '/contact', :to => 'static_pages#contact'
 
   # The priority is based upon order of creation:
